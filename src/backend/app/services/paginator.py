4506a540168a71facc178e448aec82d2fdf802ad
Saad Shaikh <heysaad@gmail.com>
2026-05-11 19:30:07 +0530

    fix obsolete

--- src/backend/app/services/paginator.py
+++ src/backend/app/services/paginator.py
@@ -5,22 +5,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.infra.data.database import get_db
 
-
-class Paginator:
-    def __init__(self, db: AsyncSession = Depends(get_db)):
-        self.db = db
-
-    async def get_paginated[TData](self, req: PaginationRequest, query: Select):
-        total_query = select(func.count()).select_from(query.subquery())
-        total = (await self.db.execute(total_query)).scalar() or 0
-
-        data_query = query.offset(
-            (req.page-1)*req.pageSize).limit(req.pageSize)
-        data = (await self.db.execute(data_query)).scalars().all()
-
-        return PaginationResponse[TData](total=total, data=data)
-
-
 class PaginationRequest[TFilter](BaseModel):
     page: int = 1
     pageSize: int = 10
@@ -37,3 +21,19 @@ class PaginationResponse[TData](BaseModel):
             data=TypeAdapter(list[typ]).validate_python(self.data),
             total=self.total
         )
+
+
+class Paginator:
+    def __init__(self, db: AsyncSession = Depends(get_db)):
+        self.db = db
+
+    async def get_paginated[TData](self, req: PaginationRequest, query: Select):
+        total_query = select(func.count()).select_from(query.subquery())
+        total = (await self.db.execute(total_query)).scalar() or 0
+
+        data_query = query.offset(
+            (req.page-1)*req.pageSize).limit(req.pageSize)
+        data = (await self.db.execute(data_query)).scalars().all()
+
+        return PaginationResponse[TData](total=total, data=data)
+
